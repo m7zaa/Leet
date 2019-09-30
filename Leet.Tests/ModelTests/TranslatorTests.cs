c8d497ec6798dfd55508b60265c1d355b2c28469
joel stockamp and micah zaayer <joel@>
2019-09-30 11:31:05 -0700

    add successful testing for all specs.

--- Leet.Tests/ModelTests/TranslatorTests.cs
+++ Leet.Tests/ModelTests/TranslatorTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 using Leet;
 namespace Leet.Tests
 {
@@ -9,7 +10,38 @@ namespace Leet.Tests
         public void TranslateETo3_LetterEBecomesNumeral3_p3n()
         {
             LeetTranslator testLeetTranslator = new LeetTranslator();
-            Assert.AreEqual("p3n", testLeetTranslator.TranslateETo3("pEn"));
+            Assert.AreEqual("p3n", testLeetTranslator.TranslateLetter("pEn"));
         }
+        [TestMethod]
+        public void TranslateOTo0_LetterOBecomesNumeral0_p0d()
+        {
+            LeetTranslator testLeetTranslator = new LeetTranslator();
+            Assert.AreEqual("p0d", testLeetTranslator.TranslateLetter("pOd"));
+        }
+        [TestMethod]
+        public void TranslateTTo7_LetterTBecomesNumeral7_7h3()
+        {
+            LeetTranslator testLeetTranslator = new LeetTranslator();
+            Assert.AreEqual("7h3", testLeetTranslator.TranslateLetter("the"));
+        }
+        [TestMethod]
+        public void TranslateITo1_LetterIBecomesNumeral1_1sle()
+        {
+            LeetTranslator testLeetTranslator = new LeetTranslator();
+            Assert.AreEqual("1zl3", testLeetTranslator.TranslateLetter("Isle"));
+        }
+        [TestMethod]
+        public void TranslateSToZ_LetterSBecomesLetterZ_snackz()
+        {
+            LeetTranslator testLeetTranslator = new LeetTranslator();
+            Assert.AreEqual("snackz", testLeetTranslator.TranslateLetter("snacks"));
+        }
+        [TestMethod]
+        public void TranslatePhraseToStrings_LetterSBecomesLetterZ_The_Apple()
+        {
+            LeetTranslator testLeetTranslator = new LeetTranslator();
+            Assert.AreEqual("7h3 snappl3z ", testLeetTranslator.SplitPhraseToWords("The snapples"));
+        }
+
     }
 }
--- Leet/Models/Leet.cs
+++ Leet/Models/Leet.cs
@@ -6,7 +6,7 @@ namespace Leet
 {
     public class LeetTranslator
     {
-        public string TranslateETo3 (string input)
+        public string TranslateLetter (string input)
         {
             char[] inputArray = input.ToCharArray();
             for (int i = 0; i<inputArray.Length; i++)
@@ -15,9 +15,41 @@ namespace Leet
                 {
                     inputArray[i] = (char) '3';
                 }
+                if (inputArray[i] == (char)'o' || inputArray[i] == (char)'O')
+                {
+                    inputArray[i] = (char)'0';
+                }
+                if (inputArray[i] == (char)'t' || inputArray[i] == (char)'T')
+                {
+                    inputArray[i] = (char) '7';
+                }
+                if (inputArray[i] == (char)'I')
+                {
+                    inputArray[i] = (char) '1';
+                }
+                
+                if (inputArray[i] == (char)'S' || inputArray[i] == (char)'s')
+                {
+                    if (i != 0)
+                    {
+                    inputArray[i] = (char)'z';
+                    }
+                }
             }
             string result = string.Join("", inputArray);
             return result;
+
+        }
+        public string SplitPhraseToWords(string input)
+        {
+            string outputPhrase= "";
+            string[] inputSplit = input.Split(" ");
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+             string Word = TranslateLetter(inputSplit[i]);    
+             outputPhrase += Word + " ";
+            }
+            return outputPhrase;
         }
     }
 }
